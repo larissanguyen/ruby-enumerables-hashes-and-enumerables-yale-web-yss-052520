79f6fab446dc2ee3f158b1ed719d797d0a9568c0
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 08:05:11 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,5 +8,11 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  passengers.each 
+  passengers.each do |suite, name|
+    if suite == :suite_a and name.start_with?("A")
+      winner = name
+    end
+  end
+  
+  winner
 end
